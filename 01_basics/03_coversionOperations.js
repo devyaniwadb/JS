96c91d37ec4c3b381b20f673faa1e63be019e7dc
Devyani wadbude <148743374+devyaniwadb@users.noreply.github.com>
2026-03-03 15:57:55 +0000

    conversion of datatypes

---
+++ 01_basics/03_coversionOperations.js
@@ -0,0 +1,11 @@
+let score = "33"
+console.log(typeof score)
+console.log(typeof(score))
+let valueInNum=Number(score)
+console.log(typeof valueInNum)
+
+// "33"=>33
+// "33abc"=>NaN not a number
+// true=>1; false =>0
+// 1=>true ,0=>false
+// ""=>false ,"divya"=>true
