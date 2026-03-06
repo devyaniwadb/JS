620e33c766b225bb6e354501bddbafe9c29f8743
Devyani wadbude <148743374+devyaniwadb@users.noreply.github.com>
2026-03-06 13:12:47 +0000

    comparision

--- 01_basics/03_coversionOperations.js
+++ 01_basics/03_coversionOperations.js
@@ -1,11 +1,33 @@
 let score = "33"
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 let valueInNum=Number(score)
-console.log(typeof valueInNum)
+// console.log(typeof valueInNum)
 
 // "33"=>33
 // "33abc"=>NaN not a number
 // true=>1; false =>0
 // 1=>true ,0=>false
 // ""=>false ,"divya"=>true
+
+// ************operations ************
+let value = 3
+let negValue=-value
+// console.log(negValue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2%2)
+// console.log(2/2)
+// console.log(2*2)
+// console.log(2**2)
+
+let str1="hello "
+let str2 ="divya"
+let str3=str1 + str2
+console.log(str3);
+console.log("1"+2);
+console.log(1+"2"); 
+
+
+
+
